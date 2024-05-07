163a6a4e6c95ba2fd5ec26bda9e31a3ed5f63838
Ammar Lakis <ammarlakis@users.noreply.github.com>
2024-05-07 16:31:20 +0200

    Update the function

--- src/index.js
+++ src/index.js
@@ -1,39 +1,46 @@
+const core = require('@actions/core');
+
 /**
  * 
  * @param {string} inputName The name of the input to fetch.
  * @param {type} types The type to test the input against.
- * @returns {any} Parsed value it passes the types list, otherwise null.
+ * @returns {any} An array of two values, the input value parsed and the type it matches, otherwise null for both.
  */
-function tryParse(inputName, ...types) {
+function parseInput(inputName, ...types) {
+  const input = core.getInput(inputName);
   for (const type of types) {
-      switch (type.toLowerCase()) {
-          case 'number':
-              const number = parseFloat(input);
-              if (!isNaN(number)) return number;
-              break;
-          case 'boolean':
-              const lowerInput = input.toLowerCase();
-              if (lowerInput === 'true') return true;
-              if (lowerInput === 'false') return false;
-              break;
-          case 'date':
-              const date = new Date(input);
-              if (!isNaN(date.getTime())) return date;
-              break;
-          case 'json':
-              try {
-                  return JSON.parse(input);
-              } catch {
-                  // Invalid JSON
-              }
-              break;
-          default:
-              console.warn(`Unsupported type: ${type}`);
-      }
+    const _type = type.toLowerCase()
+    switch (_type) {
+      case 'number':
+        const number = parseFloat(input);
+        if (!isNaN(number)) return [number, _type];
+        break;
+      case 'boolean':
+        const lowerInput = input.toLowerCase();
+        if (lowerInput === 'true') return [true, _type];
+        if (lowerInput === 'false') return [false, _type];
+        break;
+      case 'date':
+        const date = new Date(input);
+        if (!isNaN(date.getTime())) return [date, _type];
+        break;
+      case 'json':
+        try {
+          return [JSON.parse(input), _type];
+        } catch {
+          return [null, null]
+        }
+        break;
+      case 'string':
+          return [input, _type];
+        break;
+      default:
+        core.warning(`Unsupported type: ${type}`);
+    }
   }
-  return null;
+  return [null, null];
 }
 
 module.exports = {
-  tryParse
+  parseInput
 };
